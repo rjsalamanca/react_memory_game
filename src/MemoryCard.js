1922edd9d0bfdd87fd823e3b43cd68dd4d4c7c35
RJ Salamanca <salamancaronito@gmail.com>
2019-06-26 09:11:05 -0400

    added memory card css

--- src/MemoryCard.js
+++ src/MemoryCard.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 
+import './MemoryCard.css';
+
 class MemoryCard extends Component{
+    state = {
+        isFlipped: false
+    }
+
+    handleClick = async (e) => {
+        e.preventDefault();
+        this.setState({
+            isFlipped: !this.state.isFlipped
+        });
+        console.log('FLIP')
+    }
+
     render(){
+        let memoryCardInnerClass = 'MemoryCardInner';
+
+        if (!!this.state.isFlipped) {
+            memoryCardInnerClass += ' flipped';
+        }
+
         return (
-            <div className='MemoryCard'>
-                <img src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png" className="MemoryImage"/>
+
+            <div className='MemoryCard' onClick ={(e)=> this.handleClick(e)}>
+                <div className={memoryCardInnerClass}>
+                    <div className='MemoryCardBack'>
+                        <img src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png" className="MemoryImage" alt="MemoryImage"/>
+                    </div>
+                    <div className='MemoryCardFront'>∆</div>
+
+                </div>
             </div>
         );
     }
